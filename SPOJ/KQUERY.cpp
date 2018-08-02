3524a85b23ad7cc5984fefff86681a835a1421c0
RZzero <rafaelsuazoh@gmail.com>
2018-08-01 22:10:53 -0400

    ADD: SPOJ solutions

---
+++ SPOJ/KQUERY.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct Event{
+  int id, x;
+
+  
+};
+int main() {
+  int N, Q, z, x, c, dum;
+  vector<int> numbers;
+  cin >> N;
+
+  for(int i = 0; i < N; i++){
+    cin >> dum;
+    numbers.push
+  }
+  return 0;
+}
