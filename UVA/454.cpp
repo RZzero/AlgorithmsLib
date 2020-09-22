a43d1209b092ad1032a2bcca81de300a0b6867ef
RZzero <rafaelsuazoh@gmail.com>
2020-09-22 17:20:50 -0400

    WIP: 195 and 454 solutions

---
+++ UVA/454.cpp
@@ -0,0 +1,48 @@
+// https://onlinejudge.org/external/4/454.pdf
+#include <bits/stdc++.h>
+
+using namespace std;
+
+bool are_anagrams(string a, string b) {
+  sort(a.begin(), a.end());
+  sort(b.begin(), b.end());
+
+  return a == b;
+}
+
+string clean_phrase(string s) {
+  string cleaned_phrase = "";
+  for (auto c: s) {
+    if ((c >= 'a' && c <= 'z')
+      || (c >= 'A' && c <= 'Z')) {
+        cleaned_phrase += c;
+    }
+  }
+  return cleaned_phrase;
+}
+
+int main() {
+  int T;
+  cin >> T;
+  cin.ignore();
+  while (T--) {
+    string phrase;
+    vector<string> phrases;
+
+    while (getline(cin, phrase) && phrase != "") {
+      phrases.push_back(phrase);
+    }
+
+    sort(phrases.begin(), phrases.end());
+
+    for (int i = 0; i < phrases.size(); i++) {
+      for (int j = i+1; j < phrases.size(); j++) {
+        if (are_anagrams(clean_phrase(phrases[i]), clean_phrase(phrases[j]))) {
+          cout << phrases[i] << " = " << phrases[j] << endl;
+        }
+      }
+    }
+    cout << endl;
+  }
+  return 0;
+}
