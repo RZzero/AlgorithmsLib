28b6bd2b88d9357a26a61aae95f2c1d727aca170
Rafael Suazo <rafaelsuazoh@gmail.com>
2024-03-26 19:51:49 -0400

    ADD: AtCoder ABC345 C. One Time Swap Solution

--- AtCoder/abc345/c.cpp
+++ AtCoder/abc345/c.cpp
@@ -3,32 +3,31 @@ using namespace std;
 using Long = long long;
 
 int main() {
-  string s;
-  map<char, int> count_so_far;
-  string alphabet = "abcdefghijklnmopqrstuvwxyz";
+  string S;
+  cin >> S;
 
-  cin >> s;
-  count_so_far[s[0]] = 1;
-  set<char> fufua;
-  Long cnt = 0;
-  for (int i = 1; i < s.size(); i++) {
-    int local_cnt = 0;
-    int unique = 0;
-    fufua.insert(s[i]);
-    for (auto &c : alphabet) {
-      if (c == s[i] && count_so_far[c] == 1) {
-        continue;
-      }
-      if (count_so_far[c]) {
-        unique++;
+  Long answer = 0;
+  map<char, Long> count_so_far;
+  map<char, Long> prev_switch_pos;
+  string alphabet = "abcdefghijklmnopqrstuvwxyz";
+  bool extra = 0;
+  for (int i = 0; i < S.size(); i++) {
+    Long can_switch_pos = 0;
+    count_so_far[S[i]]++;
+
+    if (count_so_far[S[i]] > 1)
+      extra = 1;
+
+    for (auto &letter : alphabet) {
+      if (letter != S[i] && count_so_far.count(letter)) {
+        can_switch_pos += count_so_far[letter];
       }
     }
-    local_cnt = i * unique;
-    count_so_far[s[i]]++;
-    cnt += local_cnt;
+
+    answer += can_switch_pos - (prev_switch_pos.count(S[i]) && prev_switch_pos[S[i]] > can_switch_pos ? prev_switch_pos[S[i]] : 0);
+    prev_switch_pos[S[i]] = can_switch_pos;
   }
 
-  cnt = cnt / 2;
-  cout << 1 + (fufua.size() > 1 ? cnt : 0) << endl;
+  cout << answer + extra << endl;
   return 0;
 }
